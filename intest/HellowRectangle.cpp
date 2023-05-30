d10999048e3d4f967fb3cdee7aaab137540c69fe
MSI75 <sisaif56@gmail.com>
2023-05-30 12:43:47 +0600

    hellowRectangle

---
+++ intest/HellowRectangle.cpp
@@ -0,0 +1,30 @@
+/*
+input:
+3 5
+output :
+*****
+*   *
+*****
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
