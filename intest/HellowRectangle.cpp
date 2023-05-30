1b8a671d810f007a4f94fd64156d19225d210642
MSI75 <sisaif56@gmail.com>
2023-05-30 12:49:13 +0600

    helfPyramid

--- intest/HellowRectangle.cpp
+++ intest/HellowRectangle.cpp
@@ -19,7 +19,7 @@ int main()
     {
         for (int j = 0; j < col; j++)
         {
-            if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+            if (i == 0 || i == row-1 || j == 0 || j == col - 1)
                 cout << "*";
             else
                 cout << " ";
