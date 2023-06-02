8664b8e395a3bb3e1e084c37a54d3258d82f9a9c
MSI75 <sisaif56@gmail.com>
2023-06-02 20:55:43 +0600

    IPL Match Day 2

--- Week1/Day1/IPL_Match_DaY2.cpp
+++ Week1/Day1/IPL_Match_DaY2.cpp
@@ -3,6 +3,11 @@
 1 2 3 1 4 5 2 3 6
 3
 
+
+10
+8 5 10 7 9 4 15 12 90 13
+4
+
 */
 
 #include <bits/stdc++.h>
@@ -19,31 +24,29 @@ int main()
 
     int k;
     cin >> k;
-
-    list<int> li;
+    multiset<int> mlt;
     int i = 0, j = 0;
-    int mx = INT_MIN;
+    vector<int> v;
 
     while (j < n)
     {
-        li.push_back(arr[j]);
+        mlt.insert(arr[j]);
         if (j < k - 1)
         {
             j++;
         }
         else
         {
-            // int front = li.max_size();
-            // cout << front << "\n";
-            // mx = max(front, mx);
-            // cout << mx << " ";
-
-            auto mx = max_element(li.begin(), li.end());
-
-            mx -= arr[i];
+            auto mx_element = *mlt.rbegin();
+            v.push_back(mx_element);
+            auto it = mlt.find(arr[i]);
+            mlt.erase(it);
             i++;
             j++;
         }
     }
+
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
     return 0;
 }
