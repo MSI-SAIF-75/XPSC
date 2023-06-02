cffc74f3d158ed71d980569430664002ef5be46b
MSI75 <sisaif56@gmail.com>
2023-06-03 05:45:06 +0600

    Multiplication

---
+++ Week1/Day2/Multiplication.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int multiplication = a * b;
+    cout << multiplication << "\n";
+    return 0;
+}
