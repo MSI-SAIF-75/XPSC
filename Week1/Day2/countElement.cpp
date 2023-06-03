8ff93ab4c25be5e7548172e355c1624a4c90daf8
MSI75 <sisaif56@gmail.com>
2023-06-03 14:42:54 +0600

    count element

--- Week1/Day2/countElement.cpp
+++ Week1/Day2/countElement.cpp
@@ -21,10 +21,46 @@ int main()
     int count = 0;
     for (auto it = mp.begin(); it != mp.end(); it++)
     {
+        // equal value search in array
         auto finder = mp.find(it->first - 1);
+
+        // equal value count
         if (finder != mp.end())
             count += finder->second;
     }
     cout << count << "\n";
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+
+//     map<int, int> mp;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         mp[arr[i]]++;
+//     }
+
+//     // for(auto it : mp) cout<<it.first<<"  "<<it.second<<"\n";
+//     int count = 0;
+
+//     for (auto it : mp)
+//     {
+//         auto finder = mp.find(it.first - 1);
+
+//         if (finder != mp.end())
+//             count += finder->second;
+//     }
+//     cout << count << "\n";
+
+//     return 0;
+// }
