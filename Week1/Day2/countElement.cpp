c501ef712f1df0b0fe7a134296a82a40b3e11be3
MSI75 <sisaif56@gmail.com>
2023-06-03 16:26:26 +0600

    count element

--- Week1/Day2/countElement.cpp
+++ Week1/Day2/countElement.cpp
@@ -3,64 +3,23 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    int arr[N];
-    for (int i = 0; i < N; i++)
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
+
     map<int, int> mp;
 
-    for (int i = 0; i < N; i++)
-    {
-        if (mp.find(arr[i]) == mp.end())
-            mp[arr[i]] = 1;
-        else
-            mp[arr[i]] += 1;
-    }
+    for (int i = 0; i < n; i++)
+        mp[arr[i]]++;
 
     int count = 0;
-    for (auto it = mp.begin(); it != mp.end(); it++)
+    for (int i = 0; i < n; i++)
     {
-        // equal value search in array
-        auto finder = mp.find(it->first - 1);
-
-        // equal value count
-        if (finder != mp.end())
-            count += finder->second;
+        if (mp[arr[i] + 1] >= 1)
+            count++;
     }
     cout << count << "\n";
     return 0;
 }
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     for (int i = 0; i < n; i++)
-//         cin >> arr[i];
-
-//     map<int, int> mp;
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         mp[arr[i]]++;
-//     }
-
-//     // for(auto it : mp) cout<<it.first<<"  "<<it.second<<"\n";
-//     int count = 0;
-
-//     for (auto it : mp)
-//     {
-//         auto finder = mp.find(it.first - 1);
-
-//         if (finder != mp.end())
-//             count += finder->second;
-//     }
-//     cout << count << "\n";
-
-//     return 0;
-// }
