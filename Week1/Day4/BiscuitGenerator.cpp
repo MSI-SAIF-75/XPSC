b2a69ddbe29a73aa28bee81fe435331d0b28b62c
MSI75 <sisaif56@gmail.com>
2023-06-04 17:03:27 +0600

    BiscuitGenerator

---
+++ Week1/Day4/BiscuitGenerator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, t;
+    cin >> a >> b >> t;
+
+    int totalN = (t / a) * b;
+    cout << totalN << "\n";
+
+    return 0;
+}
