18a113dc9683ecd20292c3ed10abbbd7986bd725
MSI75 <sisaif56@gmail.com>
2023-06-10 20:07:20 +0600

    modify

--- Week2/Day1/7_Rings.cpp
+++ Week2/Day1/7_Rings.cpp
@@ -10,15 +10,21 @@ int main()
         int n, x;
         cin >> n >> x;
 
-        int t = n * x;
-        int count = 0;
-        while (t != 0)
-        {
-            int digits = t % 10;
-            count++;
-            t /= 10;
-        }
-        if (count == 5)
+        // int T = n * x;
+        // int count = 0;
+        // while (T != 0)
+        // {
+        //     int digits = T % 10;
+        //     count++;
+        //     T /= 10;
+        // }
+        // if (count == 5)
+        //     cout << "YES\n";
+        // else
+        //     cout << "NO\n";
+
+        int T = n * x;
+        if (T >= 10000 && T <= 99999)
             cout << "YES\n";
         else
             cout << "NO\n";
