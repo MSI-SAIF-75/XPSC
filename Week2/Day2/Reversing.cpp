fa20b37a4f3650c2fd16aa99a5c528dc8358fb79
MSI75 <sisaif56@gmail.com>
2023-06-10 19:58:24 +0600

    Modify

--- Week2/Day2/Reversing.cpp
+++ Week2/Day2/Reversing.cpp
@@ -6,17 +6,45 @@ int main()
     int n;
     cin >> n;
 
-    int arr[n];
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (arr[i] == 0)
+    //         reverse(arr, arr + i);
+    // }
+    // for (int i = 0; i < n; i++)
+    //     cout << arr[i] << " ";
+
+    vector<int> v(n);
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
+        cin >> v[i];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (v[i] == 0)
+    //     {
+    //         int j = i - 1;
+    //         int k = 0;
+    //         while (k <= j)
+    //         {
+    //             swap(v[k], v[j]);
+    //             j--;
+    //             k++;
+    //         }
+    //     }
+    // }
 
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] == 0)
-            reverse(arr, arr + i);
+        if (v[i] == 0)
+            reverse(v.begin(), v.begin() + i);
     }
+
     for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+        cout << v[i] << " ";
 
     return 0;
 }
