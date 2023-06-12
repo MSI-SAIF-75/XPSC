88309a1b523cec98a2935d32289fa625743eff57
MSI75 <sisaif56@gmail.com>
2023-06-12 19:48:16 +0600

    sum of digits

---
+++ Week2/Day5/Sum_of_Digits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int count = 0;
+
+    while (s.length() > 1)
+    {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++)
+            sum += s[i] - '0';
+        s = to_string(sum);
+        count++;
+    }
+    cout << count << "\n";
+    return 0;
+}
