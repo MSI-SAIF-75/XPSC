3fae1b4f254fa4532682ec99acc963fc95330952
MSI75 <sisaif56@gmail.com>
2023-06-13 20:37:33 +0600

    division

--- Contest/xpsc1/Division.cpp
+++ Contest/xpsc1/Division.cpp
@@ -12,17 +12,13 @@ int main()
         cin >> rating;
 
         if (1900 <= rating)
-            cout << "Division 1"
-                 << "\n";
+            cout << "Division 1\n";
         else if (1600 <= rating && rating <= 1899)
-            cout << "Division 2"
-                 << "\n";
+            cout << "Division 2\n";
         else if (1400 <= rating && rating <= 1599)
-            cout << "Division 3"
-                 << "\n";
+            cout << "Division 3\n";
         else
-            cout << "Division 4"
-                 << "\n";
+            cout << "Division 4\n";
     }
     return 0;
 }
