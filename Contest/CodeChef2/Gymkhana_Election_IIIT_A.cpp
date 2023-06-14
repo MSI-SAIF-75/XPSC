51103e2333fef524a794cb355880fb70546f5d2c
MSI75 <sisaif56@gmail.com>
2023-06-14 21:44:37 +0600

    Gymkhana Election IIIT A

---
+++ Contest/CodeChef2/Gymkhana_Election_IIIT_A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        int min = (m / 2) + 1;
+        cout << min << "\n";
+    }
+    return 0;
+}
