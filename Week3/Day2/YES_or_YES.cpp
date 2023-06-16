32dae963fd7980548ce2d52dfed8af7e96b246d0
MSI75 <sisaif56@gmail.com>
2023-06-16 16:01:05 +0600

    YES or YES

---
+++ Week3/Day2/YES_or_YES.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if ((s[0] == 'y' || s[0] == 'Y') && (s[1] == 'e' || s[1] == 'E') && (s[2] == 's' || s[2] == 'S'))
+            cout << "YES"
+                 << "\n";
+        else
+            cout << "NO"
+                 << "\n";
+    }
+    return 0;
+}
