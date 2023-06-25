e30e5f68dac356d0a0f88ece2b4722713a7e8613
MSI75 <sisaif56@gmail.com>
2023-06-25 10:13:23 +0600

    A + B

---
+++ Week4/Day3/A_+_b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+ll t;
+void doWork()
+{
+    int a, b;
+    cin>>a>>b;
+    int sum = a + b;
+    cout<<sum<<"\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
