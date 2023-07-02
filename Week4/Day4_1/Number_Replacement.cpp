9a2a39b562c198aa9792e5209ee09f5ada0871bd
MSI75 <sisaif56@gmail.com>
2023-07-02 22:22:07 +0600

    Number_Replacement

---
+++ Week4/Day4_1/Number_Replacement.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+ll t;
+void doWork()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    string s;
+    cin >> s;
+    bool flag = true;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j] && s[i] != s[j])
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if (flag == true)
+        cout << "YES"
+             << "\n";
+    else
+        cout << "NO"
+             << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
