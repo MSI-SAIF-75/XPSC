4b252ea78ce34e7ce305bf53c88214b428e90dd5
MSI75 <sisaif56@gmail.com>
2023-07-03 21:00:41 +0600

    decode string

---
+++ Week4/Day8/Decode_String.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+ll t;
+void doWork()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    vector<char> ans;
+    for (int i = 0; i < n; i++)
+    {
+        if ((i + 2 <= n && s[i + 2] == '0') && (i + 3 >= n || s[i + 3] != '0'))
+        {
+            int asci = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            ans.push_back(96 + asci);
+            i += 2;
+        }
+        else
+            ans.push_back(96 + (s[i] - '0'));
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i];
+    cout << '\n';
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
