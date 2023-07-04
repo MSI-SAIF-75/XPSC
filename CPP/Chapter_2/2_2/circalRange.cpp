daaba58c787d36de2bc77aa13e668b7db36b1ac1
MSI75 <sisaif56@gmail.com>
2023-07-04 23:39:04 +0600

    circalRange

---
+++ CPP/Chapter_2/2_2/circalRange.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.1416
+
+int main()
+{
+    double r;
+    cin >> r;
+
+    double range = 2 * PI * r;
+    cout << range << "\n";
+    return 0;
+}
