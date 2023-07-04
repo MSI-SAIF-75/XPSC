a1f352e5ccca4fd5f725866d08a090c6a53d8e72
MSI75 <sisaif56@gmail.com>
2023-07-04 22:13:47 +0600

    timus1000

---
+++ CPP/Chapter_2/2_2/timus1000.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+ll t;
+void doWork()
+{
+    
+}
+
+int main()
+{
+    Faster;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     doWork();
+    // }
+
+    int a, b;
+    cin>>a>>b;
+    int sum = a + b;
+    cout<<sum<<"\n";
+    return 0;
+}
