92e4d9cae23f01f279a57685619e6d2a1cc0cd57
MSI75 <sisaif56@gmail.com>
2023-07-04 23:02:44 +0600

    2d co-ordinate midpoint

---
+++ CPP/Chapter_2/2_2/2d_coordinate_midpoint.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    double m1 = (x1 + x2) / 2.0;
+    double m2 = (y1 + y2) / 2.0;
+
+    cout << m1 << "," << m2 << "\n";
+
+    return 0;
+}
