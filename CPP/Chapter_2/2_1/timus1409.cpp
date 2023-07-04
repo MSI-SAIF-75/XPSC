b0beefc2239786d533a3a7a1507a032897e98d18
MSI75 <sisaif56@gmail.com>
2023-07-04 22:45:40 +0600

    timus1409

---
+++ CPP/Chapter_2/2_1/timus1409.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+ll t;
+void doWork()
+{
+    
+}
+
+int main()
+{
+    Faster;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     doWork();
+    // }
+
+    int x, y;
+    cin>>x>>y;
+    cout<<y-1<<" "<<x-1<<"\n";
+    return 0;
+}
