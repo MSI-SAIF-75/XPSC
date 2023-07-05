a99279217d9732af4eb1a72274ec2aaea8d7527e
MSI75 <sisaif56@gmail.com>
2023-07-05 20:42:45 +0600

    2000

---
+++ Contest/codechef_5_jul_23/2000.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int mult = n * 2000;
+    int div = mult/500;
+    cout << div << "\n";
+    return 0;
+}
