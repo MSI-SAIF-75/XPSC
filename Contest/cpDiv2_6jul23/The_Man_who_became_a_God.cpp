d8a2fec78e83f419f657599c85c62eddd39d0e12
MSI75 <sisaif56@gmail.com>
2023-07-07 00:22:03 +0600

    The man who became a god

---
+++ Contest/cpDiv2_6jul23/The_Man_who_became_a_God.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+int t;
+void doWork()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> temp;
+    for (int i = 0; i < n; i++)
+    {
+        int x = abs(arr[i] - arr[i + 1]);
+        temp.push_back(x);
+    }
+
+    sort(temp.begin(), temp.end(), greater<>());
+    int sum = 0;
+    for (int i = 0; i < temp.size(); i++)
+    {
+        if (i < k)
+            continue;
+        else
+            sum += temp[i];
+    }
+    cout << sum << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
