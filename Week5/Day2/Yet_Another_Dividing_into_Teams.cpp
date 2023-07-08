b6360fbef61d6b200b6ed0828c283ddaacfe641e
MSI75 <sisaif56@gmail.com>
2023-07-08 11:48:05 +0600

    Yet Another Dividing into Teams

---
+++ Week5/Day2/Yet_Another_Dividing_into_Teams.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+int t;
+void doWork()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    int ans = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if ((a[i] - a[i - 1]) == 1)
+            ans = 2;
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
