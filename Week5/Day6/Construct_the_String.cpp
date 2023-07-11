31109b294ab245ec9ee576a37be0ec47563f61f7
MSI75 <sisaif56@gmail.com>
2023-07-12 02:15:03 +0600

    Construct the string

---
+++ Week5/Day6/Construct_the_String.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+int t;
+void doWork()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << char('a' + i % b);
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
