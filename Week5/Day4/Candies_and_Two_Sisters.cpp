8c292c4bf053c671d49a0582d0a8c490652b2145
MSI75 <sisaif56@gmail.com>
2023-07-11 11:04:57 +0600

    Candies and two sisters

---
+++ Week5/Day4/Candies_and_Two_Sisters.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+int t;
+void doWork()
+{
+    ll n;
+    cin >> n;
+
+    int i = 1, count = 0, temp = n - 1;
+    while (temp > i)
+    {
+        count++;
+        i++;
+        temp--;
+    }
+    cout << count << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
