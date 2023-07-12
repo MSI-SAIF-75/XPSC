651f0ec833aaba200ed4c00cff0740261dff3614
MSI75 <sisaif56@gmail.com>
2023-07-12 16:15:43 +0600

    Remove Smallest

---
+++ Week5/Day6/Remove_Smallest.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long
+
+int t;
+void doWork()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    bool flag = true;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] - a[i - 1] > 1)
+        {
+            flag = false;
+        }
+    }
+    if (flag == true)
+        cout << "YES"
+             << "\n";
+    else
+        cout << "NO"
+             << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
