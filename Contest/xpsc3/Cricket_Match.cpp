77ccb284579b7b6606200a037a3dfda879485761
MSI75 <sisaif56@gmail.com>
2023-07-17 22:01:58 +0600

    Cricket

---
+++ Contest/xpsc3/Cricket_Match.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+
+int t;
+void doWork()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int b = m * 6 * 6;
+
+    if (n <= b)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
