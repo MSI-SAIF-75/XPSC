ead674fdca61ba51c33840faf250423f72a602ae
MSI75 <sisaif56@gmail.com>
2023-07-18 00:05:19 +0600

    Masterchef finals

---
+++ Week6/Day5/Masterchef_finals.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+
+int t;
+void doWork()
+{
+    int n;
+    cin>>n;
+    if(n<=10)cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
