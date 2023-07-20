de54f88af0f37c78495a480f9b4f993f410d7615
MSI75 <sisaif56@gmail.com>
2023-07-20 09:28:32 +0600

    add:

---
+++ Contest/codeche_19_jul_23/Segment_Three.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+
+int t;
+void doWork()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i = 0; i<n; i++)cin>>a[i];
+
+    int i = 0; j = 0;
+    int sum = 0, count = 0;
+    while(j<n){
+        sum+=a[j];
+        if(j<)
+    }
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
