e9873a192cc05d0f1dde6951fb05569e50aa411c
MSI75 <sisaif56@gmail.com>
2023-07-20 10:51:14 +0600

    AB plus C

---
+++ Week6/Day7/AB_plus_C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+
+int t;
+void doWork()
+{
+    int n;
+    cin>>n;
+
+    if(n<2){cout<<-1<<"\n";return;}
+
+    int  = n/
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        doWork();
+    }
+    return 0;
+}
