df9985989c1f97e4354fa86b9cbe7e4ec6e5cbaa
MSI75 <sisaif56@gmail.com>
2023-07-21 12:34:02 +0600

    AND 0 sum big

---
+++ Week7/Day1/AND_0_Sum_Big.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    ll ans = 1;
+    for (int i = 0; i < k; i++)
+        ans = (ans * n) % MOD;
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
