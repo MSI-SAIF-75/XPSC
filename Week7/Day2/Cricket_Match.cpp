c02785fecca492b1973207acbd4d35ebdc99677a
MSI75 <sisaif56@gmail.com>
2023-07-22 11:09:40 +0600

    Cricket_Match

---
+++ Week7/Day2/Cricket_Match.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int r, b;
+    cin >> r >> b;
+    int ball = b * 36;
+
+    if (r <= ball)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
