63faf3710f33472f3f25fce8886a9d8aa6e02890
MSI75 <sisaif56@gmail.com>
2023-07-23 09:58:23 +0600

    Passing Marks

---
+++ Week7/Day3/Passing_Marks.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end(), greater<>());
+    int ans = INT_MAX;
+    for (int i = 0; i < k; i++)
+        ans = min(ans, a[i]);
+
+    cout << ans - 1 << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
