5edfc7f0e19f600ba8074ef438454bf587498bd7
MSI75 <sisaif56@gmail.com>
2023-07-24 09:26:52 +0600

    Distinct Opposite Sums

---
+++ Week7/Day4/Distinct_Opposite_Sums.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i += 2)
+        cout << i << ' ';
+    for (int i = n; i >= 1; i -= 2)
+        cout << i << ' ';
+
+    cout << '\n';
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
