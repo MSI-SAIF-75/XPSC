b046710ceb3b2a3ebd0be900c1026c164fd0ba98
MSI75 <sisaif56@gmail.com>
2023-07-25 08:42:13 +0600

    Just One More Episode

---
+++ Week7/Day5/Just_One_More_Episode.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD=1e9+7;
+
+int t;
+void solve()
+{
+    int n;
+    cin>>n;
+    n = n - 24;
+    if(n>0)cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
