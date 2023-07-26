6b436529da48bb0ff1b45071e68dfa603912992e
MSI75 <sisaif56@gmail.com>
2023-07-26 10:34:10 +0600

    Divide Pages

---
+++ Week7/Day6/Divide_Pages.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+
+    if (sum & 1)
+        cout << "NO\n";
+    else
+        cout << "YES\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
