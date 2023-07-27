f2be59f66660a010edc896d869febe9eba9cd0df
MSI75 <sisaif56@gmail.com>
2023-07-27 21:19:52 +0600

    The Good Array

---
+++ intest/2/The_Good_Array.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int ans = (n + k - 1) / k;
+
+    if (n % k > 1 || (n % k == 0 && k >1))
+        ans++;
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
