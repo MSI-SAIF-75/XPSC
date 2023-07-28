1bcaf9bdb26955ec43e810996425332e50ccf4f7
MSI75 <sisaif56@gmail.com>
2023-07-28 21:22:16 +0600

    Can Chef

---
+++ Week8/Day1/Can_Chef.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+
+    int a = x * 15;
+    int b = y * 2;
+
+    if (a >= b)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
