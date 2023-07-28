8fa2d196aec016fae7a50aa05baace7850af0115
MSI75 <sisaif56@gmail.com>
2023-07-28 21:17:25 +0600

    IPL Ticket Rush

---
+++ Week8/Day1/IPL_Ticket_Rush.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    if (n > m)
+        cout << n - m << "\n";
+    else
+        cout << 0 << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
