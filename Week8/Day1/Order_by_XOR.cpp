4d1e07c327d4f12efaa1cba6ccf10c9887d6b1f4
MSI75 <sisaif56@gmail.com>
2023-07-28 21:03:10 +0600

    order by xor

--- Week8/Day1/Order_by_XOR.cpp
+++ Week8/Day1/Order_by_XOR.cpp
@@ -6,26 +6,65 @@ using namespace std;
     cout.tie(0);
 #define ll long long int
 const int MOD = 1e9 + 7;
+ll t;
 
-int t;
+
+ll p(ll x, ll n)
+{
+    ll res = 1;
+    while (n)
+    {
+        if (n & 1)
+        {
+            res = (res * x);
+            n--;
+        }
+        else
+        {
+            x = (x * x);
+            n /= 2;
+        }
+    }
+    return res;
+}
+
 void solve()
 {
-    int a, b, c;
+    ll a, b, c;
     cin >> a >> b >> c;
 
-    int x = 0;
-    for (int i = 0; i <= 31; i++)
-    {
-        if ((x ^ a) < (x ^ b) && (x ^ b) < (x ^ c))
-        {
-            cout << x << "\n";
-            return;
-        }
-        x = x << 1;
-        x = x ^ 1;
-    }
+    ll cnt = 0, x = 0, y = 0, z = 0;
 
-    cout << -1 << "\n";
+    for (int i = 29; i >= 0; i--)
+    {
+        ll av = (a & (1 << i)), bv = (b & (1 << i)), cv = (c & (1 << i));
+        if (av == bv && av == cv)
+            ;
+        else if (av < min(bv, cv))
+            ;
+        else if (av > max(bv, cv))
+            cnt += p(2, i);
+        else if (cv > max(bv, av))
+            z++;
+        else if (cv < min(av, bv))
+            z++, cnt += p(2, i);
+        else if (bv < min(av, cv))
+        {
+            if (z > y)
+                cnt += p(2, i);
+        }
+        else if (bv > max(av, cv))
+        {
+            if (z > y)
+                ;
+            else
+                cnt += p(2, i);
+        }
+    }
+    if ((cnt ^ a) < (cnt ^ b) && (cnt ^ b) < (cnt ^ c))
+        cout << cnt << endl;
+    else
+        cout << -1 << endl;
 }
 
 int main()
