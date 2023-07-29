f4699d9b9b1a5a9d1877ab933d0d6d7375c37664
MSI75 <sisaif56@gmail.com>
2023-07-29 16:45:11 +0600

    Maximum Crossing Hard Version

---
+++ Week8/Day2/Maximum_Crossings_Hard_Version.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    ll ans = 0;
+    pbds<int> s;
+    map<int, int> mp;
+    for (int i = 1; i <= n; i++)
+    {
+        s.insert(a[i]);
+        ans += s.order_of_key(a[i]) + mp[a[i]];
+        mp[a[i]]++;
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
