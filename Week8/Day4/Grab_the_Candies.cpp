63bdb45d7a3107ae8bc62a4784abe19d981e569c
MSI75 <sisaif56@gmail.com>
2023-07-31 19:06:54 +0600

    Grab the Candies

---
+++ Week8/Day4/Grab_the_Candies.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int m = 0, b = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] & 1)
+            b += a[i];
+        else
+            m += a[i];
+    }
+    if (m > b)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
