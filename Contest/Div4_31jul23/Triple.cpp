70f5c3f153f9893ddef620f3e5dae42d98b154f7
MSI75 <sisaif56@gmail.com>
2023-07-31 21:23:08 +0600

    Triple

---
+++ Contest/Div4_31jul23/Triple.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        mp[x]++;
+    }
+
+    int ans = 0;
+    bool f = false;
+    for (auto i : mp)
+    {
+        if (i.second >= 3)
+        {
+            ans = max(ans, i.first);
+            f = true;
+        }
+    }
+
+    if (f == true)
+        cout << ans << "\n";
+    else
+        cout << -1 << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
