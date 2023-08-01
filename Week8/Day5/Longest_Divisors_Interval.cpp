e66cef38474add65b92e4a0c63548681d1b8df34
MSI75 <sisaif56@gmail.com>
2023-08-01 10:55:32 +0600

    Longest Divisors Interval

---
+++ Week8/Day5/Longest_Divisors_Interval.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    int res = 1;
+    while (n % res == 0)
+        res++;
+    cout << res - 1 << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
