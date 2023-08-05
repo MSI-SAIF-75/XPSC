8d4a995748ea87febddc06df85818ef76dc6921e
MSI75 <sisaif56@gmail.com>
2023-08-05 21:30:54 +0600

    Tales of a Sort

---
+++ Contest/Div2_5Aug/Tales_of_a_Sort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] <= a[i + 1])
+            continue;
+        else
+            ans = max(ans, a[i]);
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
