8e7ed4227606853303a55a6ff02e0e63e4b1d906
MSI75 <sisaif56@gmail.com>
2023-08-07 13:40:07 +0600

    Powering the Hero easy version

---
+++ Week9/Day4/Powering_the_Hero_easy_version.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    ll ans = 0;
+    priority_queue<int> pq;
+    for (int i = 0; i < n; i++)
+    {
+
+        pq.push(a[i]);
+        if (a[i] == 0)
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+    }
+    // cout << pq.size() << "\n";
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
