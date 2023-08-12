b3300b18f84c2f992c3f0dbf9a353aad70dbf671
MSI75 <sisaif56@gmail.com>
2023-08-12 10:48:28 +0600

    CQXYM Count Permutations

---
+++ Week10/Day2/CQXYM_Count_Permutations.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    ll ans = 1;
+    for (int i = 3; i <= (2 * n); i++)
+    {
+        ans *= i;
+        ans %= MOD;
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
