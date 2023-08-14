6e379e770ae01a7085d408ce176f2b12482d6c4e
MSI75 <sisaif56@gmail.com>
2023-08-14 11:08:36 +0600

    Era

---
+++ Week10/Day4/Era.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x > i)
+            ans = max(ans, abs(x - i));
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
