fdf663f3c0c5221d2e19047593d3a1daad13d1fc
MSI75 <sisaif56@gmail.com>
2023-08-15 10:42:19 +0600

    Array Eversion

---
+++ Week10/Day5/Array_Eversion.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define ll long long int
+const int MOD = 1e9 + 7;
+
+int t;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    int ans = 0, x = a[n];
+
+    for (int i = n; i > 0; i--)
+    {
+        if (x < a[i])
+        {
+            ans++;
+            x = a[i];
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
